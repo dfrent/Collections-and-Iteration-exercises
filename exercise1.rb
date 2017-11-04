30fbc3d11ca3004a5d95e3cc35238aa3bd081014
Your Name <you@example.com>
2017-11-03 21:17:48 -0400

    exercise 1 hashes/areas

--- exercise1.rb
+++ exercise1.rb
@@ -2,13 +2,25 @@ fav_colours = ["red", "blue","purple", "orange", "black"]
 fam_ages = [26, 33, 29, 25, 20]
 coin_flips = ["tails", "tails", "heads", "tails", "heads"]
 artists = ["mackenta", "gucci", "enya"]
-fav_colours = [:red, :blue, :purple, :orange, :black]
+fav_colours_2 = [:red, :blue, :purple, :orange, :black]
 
 words = {array:"Arrays are ordered, integer-indexed collections of any object.",
 		 picture:"a representation of the external form of a person or thing in art.",
 		  apple: "he round fruit of a tree of the rose family"
 		  }
 
-movies = { name:"gladiator", year: 2000 }
+movies = 
+			{ name:"gladiator", year: 2000 }
 			{ name:"Pulp Fiction", year: 1994 }
 			{ name: "i,Robot", year: 2004 }
+
+# puts coin_flips
+
+# puts fav_colours[0]
+
+ # puts fam_ages.sort
+
+ # fam_ages << 0
+ # puts fam_ages
+
+ puts " a great movie #{movies[:year]} "
