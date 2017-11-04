d13e9f6a3726aefcf02ef9df89c84058d2b0aa69
Your Name <you@example.com>
2017-11-03 20:48:32 -0400

    exercise0 hashes

--- exercise1.rb
+++ exercise1.rb
@@ -3,3 +3,12 @@ fam_ages = [26, 33, 29, 25, 20]
 coin_flips = ["tails", "tails", "heads", "tails", "heads"]
 artists = ["mackenta", "gucci", "enya"]
 fav_colours = [:red, :blue, :purple, :orange, :black]
+
+words = {array:"Arrays are ordered, integer-indexed collections of any object."
+		 image:"a representation of the external form of a person or thing in art."
+		  noun: "a word (other than a pronoun) used to identify any of a class of people, places, or things common noun, or to name a particular one of these proper noun."
+		  }
+
+movies = { name:"gladiator" year: 2000 
+			name:"Pulp Fiction" year: 1994
+			name: "i,Robot" year: 2004}
