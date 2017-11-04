16effa724280602f89d6d269608d66f026a1b702
Your Name <you@example.com>
2017-11-03 22:30:05 -0400

    exercise2 hashes/arrays

--- exercise1.rb
+++ exercise1.rb
@@ -10,10 +10,18 @@ words = {array:"Arrays are ordered, integer-indexed collections of any object.",
 		  }
 
 movies = 
-			{ name:"gladiator", year: 2000 }
-			{ name:"Pulp Fiction", year: 1994 }
-			{ name: "i,Robot", year: 2004 }
+		{ name1:"gladiator", year1: 2000}
+		 {name2:"Pulp Fiction", year2: 1994} 
+		{name3: "i,Robot", year3: 2004}
 
+cities = {name1:"toronto", population1: 3000000,
+		name2:"los Angeles", population2: 4000000,
+		name3:"nyc", population3: 8500000}
+
+fam_friends = { name1:"Clarissa", age1: 33,
+			   name2:"Stephany", age2: 29,
+		   	   name3:"Ashley", age3: 25}
+# exercise 1
 # puts coin_flips
 
 # puts fav_colours[0]
@@ -23,4 +31,21 @@ movies =
  # fam_ages << 0
  # puts fam_ages
 
- puts " a great movie #{movies[:year]} "
+ # puts " a great movie #{movies[:year]} "
+
+
+ # exercise2 
+
+   # puts fav_colours.last
+
+   # cities[:name] = "mtl"
+   # cities[:population] = 200000
+   
+
+
+# exercise3
+
+# puts artists.first
+ puts " a great movie #{movies[:name3,:year3]} "
+ puts " a great movie #{movies[:name1,:year1]} "
+  puts " a great movie #{movies[:name2,:year2]} "
