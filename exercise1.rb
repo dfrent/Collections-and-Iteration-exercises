059de2e110a9529ac665c05a6f1f0ae8a7ff4ebc
Your Name <you@example.com>
2017-11-03 23:46:00 -0400

    hashes/arrays exercise 3-4

--- exercise1.rb
+++ exercise1.rb
@@ -4,48 +4,70 @@ coin_flips = ["tails", "tails", "heads", "tails", "heads"]
 artists = ["mackenta", "gucci", "enya"]
 fav_colours_2 = [:red, :blue, :purple, :orange, :black]
 
-words = {array:"Arrays are ordered, integer-indexed collections of any object.",
-		 picture:"a representation of the external form of a person or thing in art.",
-		  apple: "he round fruit of a tree of the rose family"
-		  }
+words = {
+		:array => "Arrays are ordered, integer-indexed collections of any object.",
+		:picture => "a representation of the external form of a person or thing in art.",
+		:apple => "he round fruit of a tree of the rose family"
+		}
 
-movies = 
-		{ name1:"gladiator", year1: 2000}
-		 {name2:"Pulp Fiction", year2: 1994} 
-		{name3: "i,Robot", year3: 2004}
+movies = {
+		 "gladiator" => 2000,
+		 "Pulp Fiction" => 1994,
+		 "i,Robot" => 2004
+		 }
 
-cities = {name1:"toronto", population1: 3000000,
-		name2:"los Angeles", population2: 4000000,
-		name3:"nyc", population3: 8500000}
+cities = {
+		 "toronto" => 3000000,
+		 "los Angeles" => 4000000,
+		 "nyc" => 8500000
+		 }
 
-fam_friends = { name1:"Clarissa", age1: 33,
-			   name2:"Stephany", age2: 29,
-		   	   name3:"Ashley", age3: 25}
+fam_friends = { 
+			  "Clarissa" => 33,
+			  "Stephany" => 29,
+		   	  "Ashley" => 25
+		   	  }
+
 # exercise 1
 # puts coin_flips
 
 # puts fav_colours[0]
 
- # puts fam_ages.sort
+# puts fam_ages.sort
 
- # fam_ages << 0
- # puts fam_ages
+# fam_ages << 0
+# puts fam_ages
 
- # puts " a great movie #{movies[:year]} "
+# puts " a great movie #{movies[:year]} "
 
+# exercise2 
 
- # exercise2 
+# puts fav_colours.last
 
-   # puts fav_colours.last
+# cities["miami"] = 3000000
+# puts cities
 
-   # cities[:name] = "mtl"
-   # cities[:population] = 200000
+# coin_flips.reverse!
+# puts coin_flips
+
+# puts cities.values[2]
+
+# artists.each do |i|
+# 	puts "I think #{i} is dope."	
+# end
    
-
-
 # exercise3
 
-# puts artists.first
- puts " a great movie #{movies[:name3,:year3]} "
- puts " a great movie #{movies[:name1,:year1]} "
-  puts " a great movie #{movies[:name2,:year2]} "
+# artists.each do |i|
+# 	x = artists.index(i)
+# 	if x < 2
+# 		puts i
+# 	end
+# end
+
+movies.each do |movie, year|
+	puts "#{movie} came out #{year}"
+end
+
+
+puts fam_ages.sort!.reverse
