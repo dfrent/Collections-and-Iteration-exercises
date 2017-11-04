6937df225c45d10fb65d259934496c806019b425
Your Name <you@example.com>
2017-11-03 20:56:05 -0400

    exercise0 hashes/arrays debugged

--- exercise1.rb
+++ exercise1.rb
@@ -4,11 +4,11 @@ coin_flips = ["tails", "tails", "heads", "tails", "heads"]
 artists = ["mackenta", "gucci", "enya"]
 fav_colours = [:red, :blue, :purple, :orange, :black]
 
-words = {array:"Arrays are ordered, integer-indexed collections of any object."
-		 image:"a representation of the external form of a person or thing in art."
-		  noun: "a word (other than a pronoun) used to identify any of a class of people, places, or things common noun, or to name a particular one of these proper noun."
+words = {array:"Arrays are ordered, integer-indexed collections of any object.",
+		 picture:"a representation of the external form of a person or thing in art.",
+		  apple: "he round fruit of a tree of the rose family"
 		  }
 
-movies = { name:"gladiator" year: 2000 
-			name:"Pulp Fiction" year: 1994
-			name: "i,Robot" year: 2004}
+movies = { name:"gladiator", year: 2000 }
+			{ name:"Pulp Fiction", year: 1994 }
+			{ name: "i,Robot", year: 2004 }
